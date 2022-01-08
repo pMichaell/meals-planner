11d88827bddab03cfa1c70f6fe6edfbf0fe15ad8
PMichaell <michal.pawlowicz112@gmail.com>
2022-01-09 00:38:20 +0100

    onAuthStateChanged not working in App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
 import Layout from "./ui/Layout";
 import {Route, Routes} from "react-router-dom";
 import MainPage from "./pages/MainPage";
-import {Fragment} from "react";
+import {Fragment, useEffect} from "react";
 import LoginPage from "./pages/LoginPage";
-import Footer from "./ui/Footer/Footer";
+import Footer from "./ui/Footer/Footer"
+import {onAuthStateChanged} from "firebase/auth";
+import {setActiveUser, setUserLoggedOut} from "./store/userSlice";
+import { getAuth } from "firebase/auth";
+import {useDispatch} from "react-redux";
+
 
 function App() {
+    const auth = getAuth();
+    const dispatch = useDispatch();
+
+ useEffect(() => {
+     onAuthStateChanged(auth, user => {
+         if (user) {
+         alert("onAuthStateChanged login")
+             const userEmail = user.email;
+             dispatch(setActiveUser({userEmail}))
+         }
+         else {
+             alert("onAuthStateChanged not logged in")
+             dispatch(setUserLoggedOut())
+         }
+     })
+ }, [auth, dispatch])
+
   return (
       <Fragment>
         <Layout>
